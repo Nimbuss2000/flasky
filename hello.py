a129a7dff3b070537f482ac3969ad4702e5393aa
zoings <alexl51095@mail.ru>
2023-12-11 03:29:56 +0300

    pushing

---
+++ hello.py
@@ -0,0 +1,55 @@
+from flask import Flask, request, render_template, session, redirect, url_for
+from flask_moment import Moment
+from flask_bootstrap import Bootstrap
+from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import InputRequired
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+
+class NameForm(FlaskForm):
+    name = StringField('Name?', validators=[InputRequired()])
+    submit = SubmitField('Submit')
+
+
+class SecreterForm(FlaskForm):
+    salt = StringField('Salt pls', validators=[InputRequired()])
+    cooka = StringField('Cookie value', validators=[InputRequired()])
+    submit = SubmitField('Decode')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    mc = request.cookies
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
