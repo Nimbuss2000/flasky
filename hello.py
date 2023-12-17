437abe0b3d379ab9f2a7cf76e2486b2029105072
zoings <alexl51095@mail.ru>
2023-12-17 20:46:08 +0300

    last vers

--- hello.py
+++ hello.py
@@ -28,7 +28,6 @@ class SecreterForm(FlaskForm):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
-    mc = request.cookies
     if form.validate_on_submit():
         session['name'] = form.name.data
         return redirect(url_for('index'))
